42441505447c5c42006ec4d07bfb32f4d2c7bccd
luisherreralg <lherreralg@outlook.es>
2022-10-10 23:36:10 +0200

    Fixed several code smell, added favicons+metadesc

---
+++ JS-Projects/Airlines_JS/utils.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+export const alertGetUser = () => {
+    alert('Por favor, introduzca un nombre de usuario válido');
+    getUser();
+};
+
+export const alertAskForRole = () => {
+    alert('Por favor introduce un rol válido');
+    askForRole();
+};
+
+export const alertAskForAdmin = () => {
+    alert('Por favor introduzca una acción válida');
+    askAdmin();
+};
+
+export const alertIsNan = () => {
+    alert('Por favor, introduzca una cifra válida');
+    userActions();
+};
+
+export const alertAdminActions = () => {
+    alert('Introduzca una respuesta válida');
+    askAdminMoreActions();
+};
